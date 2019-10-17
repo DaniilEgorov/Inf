65b06c40b3f6e102c541c3aa710d1d0f4101e0ec
DaniilEgorov <55592162+DaniilEgorov@users.noreply.github.com>
2019-10-17 20:55:46 +0300

    Update Task4.java

--- Lab4/Task4.java
+++ Lab4/Task4.java
@@ -10,21 +10,22 @@ public class Task4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String string = scanner.nextLine();
-        twelfth(string);
+        fourth(string);
     }
     //abcd111111102019
     public static void first_a(String string){
         System.out.println(Pattern.matches("abcd1{7}02019",string));
     }
-    //dasd123,4asd123.356adsdh87,3;kjdfg3vk56
+    //dash123,4asd123.356adsdh87,3;keefe3vk56
     public static void first_b(String string){
         Pattern pattern = Pattern.compile("\\d*\\d");
         Matcher matcher = pattern.matcher(string);
+        System.out.println(matcher.toString());
         long sum = 0 ;
         matcher.find();
         long max = Long.parseLong(string.substring(matcher.start(), matcher.end()));
-        matcher.reset();
         ArrayList<Long> arrayList = new ArrayList<>();
+        arrayList.add(max);
         while (matcher.find()){
             long i = Long.parseLong(string.substring(matcher.start(), matcher.end()));
             if(i>max)max=i;
@@ -32,9 +33,9 @@ public class Task4 {
             System.out.println(i);
             arrayList.add(i);
         }
-        System.out.println(sum);
-        System.out.println(max);
-        System.out.println(arrayList.indexOf(max));
+        System.out.println("Сумма: "+sum);
+        System.out.println("Максимальное: "+max);
+        System.out.println("Индекс максимального: "+arrayList.indexOf(max));
     }
     public static void first_c(String string){
         Pattern pattern = Pattern.compile("\\d*([.,]\\d*|\\d)");
@@ -96,6 +97,7 @@ public class Task4 {
     public static void first_i(String string){
         String [] s = string.split(" ");
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> cur1 = new ArrayList<>(),cur2 = new ArrayList<>(),cur3 = new ArrayList<>();
         System.out.print("Конвертировать все цены в (USD|RUR|EU): ");
         ArrayList<String> currencies= new ArrayList<>();
         currencies.add("USD");
@@ -104,6 +106,9 @@ public class Task4 {
         String str = scanner.next();
         double k,l;
         currencies.remove(str);
+        cur1.add(str);
+        cur2.add(currencies.get(0));
+        cur3.add(currencies.get(1));
         System.out.print("Чему равен 1 "+currencies.get(0)+" в "+str+":");
         k = scanner.nextDouble();
         System.out.print("Чему равен 1 "+currencies.get(1)+" в "+str+":");
@@ -113,28 +118,39 @@ public class Task4 {
             if (s[i].equals(str)){
                 double a = Double.parseDouble(s[i-1]);
                 if ((a*100)%1==0){
+                    cur1.add(String.valueOf(a));
                     string_new.append(String.format("%.2f %s ", a, str));
                 }
             }
             else if (s[i].equals(currencies.get(0))){
                 double a = Double.parseDouble(s[i-1]);
-                if ((a*100)%1==0)
+                if ((a*100)%1==0){
+                    cur2.add(String.valueOf(a));
                     string_new.append(String.format("%.2f %s ", a*k, str));
+                }
             }
             else if (s[i].equals(currencies.get(1))){
                 double a = Double.parseDouble(s[i-1]);
-                if ((a*100)%1==0)
+                if ((a*100)%1==0){
+                    cur3.add(String.valueOf(a));
                     string_new.append(String.format("%.2f %s ", a*l, str));
+                }
             }
         }
         System.out.println(string_new);
+        System.out.println(cur1);
+        System.out.println(cur2);
+        System.out.println(cur3);
     }
     //01:23:45:67:89:Az
     public static void third(String string){
         System.out.println(Pattern.matches("([a-fA-F0-9]{2}:){5}[a-fA-F0-9]{2}",string));
     }
+    //https://ru.wikipedia.org:2347/wiki/URL#rrrf
     public static void fourth(String string){
+        System.out.println(Pattern.matches("(https?://)?([a-zA-Z0-9])(([a-zA-Z0-9-])+\\.)+([a-z]{2,6})(:\\d+)?((/[a-zA-Z0-9]+)?)+/?(#[a-zA-Z0-9]+)?",string));
     }
+    // 01.01.2001 29/02/2000 13/1/2900
     public static void sixth(String string){
         Pattern pattern = Pattern.compile("(0[1-9]|[12]\\d|3[01])/([0][1-9]|1[0-2])/((1[6-9]|[2-9]\\d)\\d{2})");
         Matcher matcher = pattern.matcher(string);
@@ -154,14 +170,17 @@ public class Task4 {
         }
         else System.out.println(false);
     }
+    //sabina-milashka@mail.ru pavel-durov@vkontakte.ru
     public static void seventh(String string){
         System.out.println(Pattern.matches("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}",string));
     }
+    //160.95.0.0 139.17.54.23
     public static void eighth(String string){
         System.out.println(Pattern.matches(
                 "(((2((5[0-5])|([0-4]\\d)))|(1?\\d?\\d))\\.){3}((2((5[0-5])|([0-4]\\d)))|(1?\\d?\\d))"
                 ,string));
     }
+    //TQDasdf_aw31 asdd2r323_ FWADWD_123
     public static void ninth(String string){
         Pattern pattern = Pattern.compile("[\\w_]{8,}");
         Matcher matcher = pattern.matcher(string);
@@ -179,5 +198,21 @@ public class Task4 {
     }
     public static void tenth(String string){
         System.out.println(Pattern.matches("[1-9]\\d{5}",string));
+    }
+    //(3+5)-9*4
+    public static void twelfth(String string){
+        Pattern pattern = Pattern.compile("\\d[^+]");
+        Matcher matcher = pattern.matcher(string);
+        System.out.println(matcher.find());
+    }
+    public static void thirteenth(String string){
+        Pattern pattern = Pattern.compile("\\(");
+        Pattern pattern1 = Pattern.compile("\\)");
+        Matcher matcher = pattern.matcher(string);
+        int k=0,l=0;
+        while (matcher.find())k++;
+        matcher = pattern1.matcher(string);
+        while (matcher.find())l++;
+        System.out.println(k==l);
     }
 }
